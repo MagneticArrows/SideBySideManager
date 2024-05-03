3e374353f104104b513c61d274ea043ed51002c7
ArielSha111 <ArielSharkie1@gmail.com>
2024-05-03 15:00:22 +0300

    cleanup

---
+++ SideBySideManager/SideBySideManager/Comparison/ComparisonAuditManager.cs
@@ -0,0 +1,11 @@
+﻿using SideBySideManagerNuget.Comparison;
+
+namespace SideBySideManagerNuget.DataAuditor;
+
+public class DefaultComparisonAuditManager : IComparisonAuditManager
+{
+    public async Task AuditComparisonObject<T, E>(T comparisonObject) where T : BaseComparisonObject<E>
+    {
+        Console.WriteLine($"Responses equality status is: {comparisonObject.AreEqual}");//for example log warn when not equal
+    }
+}
