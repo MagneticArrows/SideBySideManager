d1b2ae9c01ffc9da906dd3ef5856183343773791
ArielSha111 <ArielSharkie1@gmail.com>
2024-05-03 14:37:43 +0300

    comparison and cleanup

--- SideBySideManager/Contracts/Http/SomeServiceResponse.cs
+++ SideBySideManager/Contracts/Http/SomeServiceResponse.cs
@@ -2,4 +2,8 @@
 
 public class SomeServiceResponse
 {
+    public int SomeInt { get; set; } = 1;
+    public string SomeString { get; set; } = "some string";
+    public double SomeDouble { get; set; } = 1.1;
+    public DateTime SomeDateTime { get; set; } = DateTime.Now;
 }
