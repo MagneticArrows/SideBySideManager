043a8fae07205c746788e73a30fa0aee96882307
ArielSha111 <ArielSharkie1@gmail.com>
2024-05-03 14:31:50 +0300

    SideBySideManager cleanup

--- SideBySideManager/SideBySideManager/SideBySideManager/SideBySideManager.cs
+++ SideBySideManager/SideBySideManager/SideBySideManager/SideBySideManager.cs
@@ -1,52 +1,40 @@
 ﻿namespace SideBySideManagerNuget;
 
 public class SideBySideManager(IComparisonManager comparisonManager) : ISideBySideManager
-{//todo cleanup
-    public async Task<T> RunSideBySideAsync<T>(Func<Task<T>> task1, Func<Task<T>> task2, bool runParallel = true, bool breakFlow = false)
+{
+    public async Task<T> RunSideBySideAsync<T>(Func<Task<T>> task1, Func<Task<T>> task2,
+        bool runParallel = true, bool breakFlow = false)
     {
-        if (breakFlow)
+        if (breakFlow)     
+            return await RunSideBySideAsync(task1, task2, runParallel);
+
+        var task2WithNoExceptions = async () =>
         {
-            if (runParallel)
+            try
             {
-                var results = await Task.WhenAll(task1(), task2());
-                comparisonManager.CompareAndAudit(results[0], results[1]);
-                return results.First();
+                return await task2();
             }
+            catch
+            {
+                return default;
+            }
+        };
 
-            var res1 = await task1();
-            var res2 = await task2();
-            comparisonManager.CompareAndAudit(res1, res2);
-            return res1;
-        }
-        else
+        return await RunSideBySideAsync(task1, task2WithNoExceptions, runParallel);            
+    }
+
+    private async Task<T> RunSideBySideAsync<T>(Func<Task<T>> task1, Func<Task<T>> task2, bool runParallel)
+    {
+        if (runParallel)
         {
-            var task2WithNoExceptions = async () =>
-            {
-                try
-                {
-                    return await task2();
-                }
-                catch
-                {
-                    return default;
-                }
-            };
-
-            if (runParallel)
-            {
-                var results = await Task.WhenAll(task1(), task2WithNoExceptions());
-                if(results.Last() is not null)
-                    comparisonManager.CompareAndAudit(results[0], results[1]);
-
-                return results.First();
-            }
-
-            var res1 = await task1();
-            var res2 = await task2WithNoExceptions();
-
-            if (res2 is not null)
-                comparisonManager.CompareAndAudit(res1, res2);
-            return res1;
+            var results = await Task.WhenAll(task1(), task2());
+            await comparisonManager.CompareAndAudit(results[0], results[1]);
+            return results.First();
         }
+
+        var res1 = await task1();
+        var res2 = await task2();
+        await comparisonManager.CompareAndAudit(res1, res2);
+        return res1;
     }
 }
