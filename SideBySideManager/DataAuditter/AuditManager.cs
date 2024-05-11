11ad514c1062e6517cec9cc6e20085f78592739f
GalBorn <galborn@gmail.com>
2024-05-11 20:56:02 +0300

    todo

--- SideBySideManager/DataAuditter/AuditManager.cs
+++ SideBySideManager/DataAuditter/AuditManager.cs
@@ -12,6 +12,7 @@ public class AuditManager : IAuditManager<IMongoClient>
         MongoClient = mongoClient;
     }
 
+    // todo audit the data to a newly created collection for audit
     public async Task AuditComparisonObject<T, E>(T comparisonObject) where T : BaseComparisonObject<E>
     {
         MongoClient.GetDatabase("Test Collection");
